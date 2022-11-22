0e17163d4321bca69770c86aba66484d870f003d
Ioannis Daniil <jondan97@gmail.com>
2022-11-22 18:25:31 +0200

    added bootstrap, created initial component structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./modules/home/home.component";
 import {AboutUsComponent} from "./modules/about-us/about-us.component";
+import {JokesListComponent} from "./modules/jokes-list/jokes-list.component";
 
 const routes: Routes = [
   {path: "home", component: HomeComponent},
+  {path: "jokeslist", component: JokesListComponent},
   {path: "aboutus", component: AboutUsComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './modules/home/home.component';
 import { AboutUsComponent } from './modules/about-us/about-us.component';
 import {RouterModule} from "@angular/router";
+import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
+import { JokesListComponent } from './modules/jokes-list/jokes-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    FooterComponent,
+    HeaderComponent,
+    JokesListComponent
   ],
   imports: [
     BrowserModule,
